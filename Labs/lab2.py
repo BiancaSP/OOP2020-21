9ba5783ddabdcfd18e1f11619913a893960a9c3b
Arshie99 <shaharshad57@gmail.com>
2020-10-27 16:21:56 +0000

    mylab2 solution

--- Labs/lab2.py
+++ Labs/lab2.py
@@ -15,23 +15,35 @@ class Types_and_Strings:
 
         #
         # Enter your own print statements below:
-        #
+        print("Hello how are you?")
 
         # print only first and last of the sentence:
-
+        print(message[0])#first
+        print(message[-1])#last
 
         # use slice notation:
+        print(message[3:]) #print from third to last position
+
+        print(message[1:5])#print from first to fifth position
+
+        print(message[:])#print everything using slice notation
+
 
 
         # escaping a character:
-
+        print("He said \"that\'s fantastic\" !")
 
         # find all a's in the input word and count how many there are:
-
+        lower_case = message.lower()
+        print(lower_case)
+        print("The first occurence of a is at position: " + str(lower_case.find('a')))
+        print("There are " + str(lower_case.count('a')) + " a's in the word.")
+        print("Total character count is: " + str(len(lower_case)))
 
         # replace all occurences of the character a with the - sign
         # try this first by assignment of a location in a string and
         # observe what happens, then use replace():
+        print(lower_case.replace('a', '_'))
 
 
     def play_with_lists(self):
@@ -39,27 +51,43 @@ class Types_and_Strings:
         print("Originally entered: "+ message)
 
         # hand the input string to a list and print it out:
+        list_message = message.split(" ")
 
+        print(list_message)
 
         # append a new element to the list and print:
+        list_message.append('good')
 
+        print(list_message)
 
         # remove from the list in 3 ways:
+        #pop method
+        list_message.pop(0)
+        print(list_message)
 
+        #del
+        del list_message[-1:-3]
+        print(list_message)
+
+        #remove
+        list_message.remove('good')
+        print(list_message)
 
         # check if the word cake is in your input list:
-
+        print('cake' in list_message)
 
         # reverse the items in the list and print:
-
+        list_message.reverse()
+        print(list_message)
 
         # reverse the list with the slicing trick:
-
+        list_message[::-1]
+        print(list_message)
 
         # print the list 3 times by using multiplication:
-
+        print(list_message * 3)
 
 
 tas = Types_and_Strings()
-tas.play_with_strings()
-#tas.play_with_lists()
+#tas.play_with_strings()
+tas.play_with_lists()
